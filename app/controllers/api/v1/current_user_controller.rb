8e418b70ee744967fe06ae84b80916f248c1c898
Justas <labanauskas.justas@gmail.com>
2023-02-18 23:48:44 +0200

    fix typo

--- app/controllers/api/v1/current_user_controller.rb
+++ app/controllers/api/v1/current_user_controller.rb
@@ -1,4 +1,4 @@
-class CurrentUserController < ApplicationController
+class Api::V1::CurrentUserController < ApplicationController
   before_action :authenticate_user!
   def index
     render json: UserSerialzier.new(current_user).serializeable_hash[:data][:attributes], status: :ok
